c6f8f7e376d52b633dd53dc17d4529920301d62f
chopdadeep <deep3254c@gmail.com>
2025-06-19 21:21:55 +0530

    add atlas setup

--- config/db.js
+++ config/db.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 const main = async () => {
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/game");
-    } catch(e){
-        console.log("connection error",e)
+        await mongoose.connect("mongodb+srv://deepchopda01:laI99UV9ePw6Ran2@cluster0.ihb1e2w.mongodb.net/");
+        console.log("Mongodb connection sucess")
+    } catch (e) {
+        console.log("connection error", e)
     }
 }
 main();
